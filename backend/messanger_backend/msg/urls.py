197337fbfc175b0429db8d9e5824c8e9a3bc6c08
SergeyVorotiy <95651513+SergeyVorotiy@users.noreply.github.com>
2022-09-25 21:54:14 +0300

    Added viewSets for chat and messsage

--- backend/messanger_backend/msg/urls.py
+++ backend/messanger_backend/msg/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 
+from rest_framework import routers
+
+from .views import ChatViewSet, MessageViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'chat', ChatViewSet)
+router.register(r'message', MessageViewSet)
 urlpatterns = [
-
+    path('', include(router.urls)),
+    path('auth-api/', include('rest-framework', namespace='rest_framework.urls'))
 ]
--- backend/messanger_backend/msg/views.py
+++ backend/messanger_backend/msg/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 
+from .models import ChatModel, MessageModel
+from .serializers import ChatSerializer, MessageSerializer
 
+
+class ChatViewSet(viewsets.ModelViewSet):
+    model = ChatModel
+    serializer_class = ChatSerializer
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    model = MessageModel
+    serializer_class = MessageSerializer
