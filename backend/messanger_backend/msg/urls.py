346241ea5f8e5ecc7396ff8ce3f0bc2ee95468bd
SergeyVorotiy <95651513+SergeyVorotiy@users.noreply.github.com>
2022-09-26 03:39:29 +0300

    reorganized url

--- backend/messanger_backend/msg/urls.py
+++ backend/messanger_backend/msg/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path, include
 
-from rest_framework import routers
-
-from .views import ChatViewSet, MessageViewSet, chat_list, chat_detail, chat_messages
+from .views import chat_list, chat_detail, chat_messages
 
 
-router = routers.DefaultRouter()
-router.register(r'chatinggggrouter', ChatViewSet)
-router.register(r'message', MessageViewSet)
 urlpatterns = [
-    # path('', include(router.urls)),
     path('chat_list/', chat_list),
     path('chat_list/<int:pk>/', chat_detail),
     path('chat_list/<int:pk>/messages/', chat_messages),
