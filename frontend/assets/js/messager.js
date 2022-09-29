fdad6f014cff15892fb4116835e2412eb418d5f2
SergeyVorotiy <95651513+SergeyVorotiy@users.noreply.github.com>
2022-09-29 15:46:50 +0300

    Added responses without websocket

--- frontend/assets/js/messager.js
+++ frontend/assets/js/messager.js
@@ -1,11 +1,13 @@
-
+let userListStorage = []
+let chatListStorage = []
+let chatMessageListStorage = {}
 function useRequest (method, url, body = null) {
     return new Promise(((resolve, reject) => {
         const userToken = window.sessionStorage.getItem('Authorization')
         const xhr = new XMLHttpRequest()
         xhr.open(method, url)
         xhr.responseType = 'json'
-        xhr.setRequestHeader('Content-Type', 'application/json', 'Access-Control-Allow-Origin', '*')
+        xhr.setRequestHeader('Content-Type', 'application/json')
         xhr.setRequestHeader('Authorization', userToken)
         xhr.onload = () => {
             if (xhr.status >= 400) {
@@ -25,22 +27,41 @@ function displayMessageList(chaID){
     let messageList = document.getElementById('message-block')
     useRequest('GET', 'http://127.0.0.1:8000/chat_list/'+chaID+'/messages/').then(message_list => {
         let messageSTR = ''
+        getUsers()
         message_list.forEach(message => {
-            messageSTR += `
-            <p>${message.author}: ${message.text}</p>
-            `
+            let username = ''
+            userListStorage.forEach(user => {
+                if (user.id == message.author){
+                    username = user.username
+                }
+            })
+            if (username != '') {
+                messageSTR += `
+                <p>${username}: ${message.text}</p>
+                `
+            }else{
+                messageSTR += `
+                <p>Я: ${message.text}</p>
+                `
+            }
         })
         messageList.innerHTML = messageSTR
-
+        chatMessageListStorage['chatID'] = message_list
+        console
+        let id = 'Chat_'+chaID
+        let activeChat = document.getElementById(id)
+        let sendingForm = document.getElementById('send-message-form')
+        sendingForm.setAttribute('class', 'displayed')
+        activeChat.setAttribute('class', 'chat-item active_chat')
     })
 }
 function sendMessage(chat_id, text){
-    useRequest('POST', 'http://127.0.0.1:8000/chat_list/'+chat_id+'/messages/', {text:text}).then(data => displayMessageList(chat_id))
-
+    useRequest('POST', 'http://127.0.0.1:8000/chat_list/'+chat_id+'/messages/', {text:text}).then(() => displayMessageList(chat_id))
 }
 function display_chat_list(){
     let chatList = document.getElementById('chat-items')
     useRequest('GET', 'http://127.0.0.1:8000/chat_list/').then(chat_list => {
+        getUsers()
         let chatString = ''
         chat_list.forEach(chat => {
             chatString += `
@@ -53,18 +74,27 @@ function display_chat_list(){
         chatList.querySelectorAll('.chat-item').forEach(item => {
             const chat_id = +item.id.substr(item.id.indexOf('_') + 1)
             item.addEventListener('click', () => {
-                displayMessageList(chat_id)
-                let sendingForm = document.getElementById('send-message-form')
-                sendingForm.setAttribute('class', 'displayed')
+                let activeChat = chatList.querySelector('.active_chat')
+                if (activeChat){
+                    activeChat.setAttribute('class', 'chat-item')
+                }
+
+
                 window.sessionStorage.setItem('Active_chat', chat_id)
+                displayMessageList(window.sessionStorage['Active_chat'])
+
             })
         })
+        if (window.sessionStorage['Active_chat']) {
+            displayMessageList(window.sessionStorage['Active_chat'])
+        }
         sendButton = document.getElementById('send-message-button')
         sendButton.addEventListener('click', () => {
             let text = document.getElementById('text-message')
             sendMessage(window.sessionStorage.getItem('Active_chat'), text.value)
             text.value = ''
         })
+        chatListStorage = chat_list
     })
 }
 function createChat() {
@@ -84,7 +114,13 @@ function createChat() {
         chat_name:chatName,
         recipients:recipients
     }
-    useRequest('POST', 'http://127.0.0.1:8000/chat_list/', request).then(data => display_chat_list())
+    useRequest('POST', 'http://127.0.0.1:8000/chat_list/', request).then(() => display_chat_list())
+}
+function getUsers(){
+    useRequest('GET', 'http://127.0.0.1:8000/user_list/')
+        .then(userListResponse => {
+            userListStorage = userListResponse
+            })
 }
 const newChatButton = document.getElementById('new-chat-button')
 newChatButton.addEventListener('click', () => {
@@ -93,15 +129,15 @@ newChatButton.addEventListener('click', () => {
     userListView.setAttribute('class', 'user-list-view')
     let userlist = []
     useRequest('GET', 'http://127.0.0.1:8000/user_list/')
-        .then(userlistResponse => {
-            userlist = userlistResponse
+        .then(userListResponse => {
+            userlist = userListResponse
             let userViewStr = `<div id="create_chat_form"><input id="set_chat_name" type="text" value="New Chat">`
             userlist.forEach(user => {
                 if (user.avatar){
                     userViewStr += `
                 <div id="user_${user.id}" class="user_card">
                 <label for="check_user_${user.id}"><input class="user_checkBox" id="check_user_${user.id}" type="checkbox">
-                <img src="http://127.0.0.1:8000${user.avatar}"><p>${user.username}</p></label>
+                <img src="http://127.0.0.1:8000${user.avatar}" alt="avatar"><p>${user.username}</p></label>
                 </div>
                 `
                 }else{
@@ -117,8 +153,8 @@ newChatButton.addEventListener('click', () => {
                             <button id="cancel_created_chat">Отменить</button></div>`
 
             userListView.innerHTML = userViewStr
-            document.body.insertBefore(userListView, document.querySelector('content'))
-            let checkboxes = userListView.querySelectorAll('.user_checkBox')
+            document.body.insertBefore(userListView, document.querySelector('.content'))
+            // let checkboxes = userListView.querySelectorAll('.user_checkBox')
             const creationButton = document.getElementById('Created_chat')
             creationButton.addEventListener('click', () => {
                 createChat()
@@ -129,11 +165,10 @@ newChatButton.addEventListener('click', () => {
             cancelButton.addEventListener('click', () => {
                 document.body.removeChild(userListView)
             })
-
-})
+            userListStorage = userListResponse
+        })
         .catch(err => console.log(err))
 })
 
-
-
 display_chat_list()
+
