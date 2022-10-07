230a91521ab4df143fa95104ef240a7e36211eca
VorotySergey <95651513+SergeyVorotiy@users.noreply.github.com>
2022-10-07 16:38:39 +0300

    finished porgect

--- pythonProject1/websocketlerning/chat/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .consumers import ChatConsumer
-
-
-ws_urlpatterns = [
-    path('ws/chat/<room_name>/', ChatConsumer.as_asgi())
-]
--- pythonProject1/websocketlerning/chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import chat, room
-
-
-urlpatterns = [
-    path('', chat),
-    path('<str:room_name>/', room)
-]
